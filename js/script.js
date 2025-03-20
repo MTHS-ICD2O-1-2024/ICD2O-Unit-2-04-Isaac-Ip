76e4cbfd97dbbca3354284d3689b44f6749d07e7
default <isaac.ip@stu.ocsb.ca>
2025-03-20 03:11:19 +0000

    made unit 2-04

--- js/script.js
+++ js/script.js
@@ -1,12 +1,22 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2025 Isaac Ip All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Isaac Ip
+// Created on: Mar 2025
 // This file contains the JS functions for index.html
 
+"use strict"
+
 /**
- * Displays an alert with the message "Hello, World!"
+ * This function calculates area of a triangle.
  */
-function myButtonClicked() {
-  alert("Hello, World!")
+function calculateAreaOfTriangle () {
+  // input
+  const baseLengthOfTriangle = parseInt(document.getElementById('base-length-of-triangle').value)
+  const heightOfTriangle = parseInt(document.getElementById('height-of-triangle').value)
+
+  // process
+  const areaOfTriangle = (baseLengthOfTriangle * heightOfTriangle) / 2
+
+  // output
+  document.getElementById('area').innerHTML = 'Area is: ' + areaOfTriangle + ' cm²'
 }
